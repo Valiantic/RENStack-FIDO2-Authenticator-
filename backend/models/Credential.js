35ad539ab2e36dbccc054fedb46b368048bfac66
StevenMadali <stevenmadali17@gmail.com>
2025-03-11 18:24:21 +0800

    Fix typo on Credential.js

--- backend/models/Credential.js
+++ backend/models/Credential.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 const User = require('./User');
 
-const Credential = sequelize.define('Credential', {
+const Credential = sequelize.define('credential', {
   credentialId: {
     type: DataTypes.STRING(512),
     allowNull: false,
